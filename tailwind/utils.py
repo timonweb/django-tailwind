5f39c9dd90d9a2629b0a9a7673a33e5c099a746a
Thomas Matecki <thomas.matecki@gmail.com>
2020-08-04 23:28:44 -0400

    Use app_label to lookup django app in Registry
    Rather than app_name.

--- tailwind/utils.py
+++ tailwind/utils.py
@@ -6,7 +6,9 @@ DJANGO_TAILWIND_APP_DIR = os.path.dirname(__file__)
 
 
 def get_app_path(app_name):
-    return apps.get_app_config(app_name).path
+    def get_app_path(app_name):
+    app_label = app_name.split('.')[-1]
+    return apps.get_app_config(app_label).path
 
 
 def get_tailwind_src_path(app_name):
