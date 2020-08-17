3bac50080eb5790bde34cb88176fa194fa1a9932
Tim Kamanin <tim@timonweb.com>
2020-08-17 23:17:09 +0200

    Updates Changelog and tailwind.config.js

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+Changelog
+=========
+
+0.10.0
+---------
+* Adds Django 3.1 support
+* Adds Purge CSS support and instructions
+* Fixes failing tests, we're green now!
+* Updates tailwindcss to 1.6.2
+* Updates tailwind.config.js
+
+0.9.0
+---------
+* Updates script commands in package.json
+* Fixes SASS support
+
+0.8.0
+---------
+* Updates tailwindcss to 1.4.6
+* Updates tailwind.config.js
+  
+0.7.0
+---------
+* Updates tailwindcss to 1.2.0
+* Updates tailwind.config.js
+
+0.6.1
+---------
+* Updates tailwindcss to 1.1.4
+* Updates other dependencies in package.json
--- tailwind/app_template/static_src/tailwind.config.js
+++ tailwind/app_template/static_src/tailwind.config.js
@@ -622,6 +622,34 @@ module.exports = {
       '700': '700ms',
       '1000': '1000ms',
     },
+    animation: {
+      none: 'none',
+      spin: 'spin 1s linear infinite',
+      ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+      pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      bounce: 'bounce 1s infinite',
+    },
+    keyframes: {
+      spin: {
+        to: { transform: 'rotate(360deg)' },
+      },
+      ping: {
+        '75%, 100%': { transform: 'scale(2)', opacity: '0' },
+      },
+      pulse: {
+        '50%': { opacity: '.5' },
+      },
+      bounce: {
+        '0%, 100%': {
+          transform: 'translateY(-25%)',
+          animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+        },
+        '50%': {
+          transform: 'none',
+          animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+        },
+      },
+    },
   },
   variants: {
     accessibility: ['responsive', 'focus'],
@@ -630,6 +658,7 @@ module.exports = {
     alignSelf: ['responsive'],
     appearance: ['responsive'],
     backgroundAttachment: ['responsive'],
+    backgroundClip: ['responsive'],
     backgroundColor: ['responsive', 'hover', 'focus'],
     backgroundOpacity: ['responsive', 'hover', 'focus'],
     backgroundPosition: ['responsive'],
@@ -643,6 +672,7 @@ module.exports = {
     borderWidth: ['responsive'],
     boxShadow: ['responsive', 'hover', 'focus'],
     boxSizing: ['responsive'],
+    container: ['responsive'],
     cursor: ['responsive'],
     display: ['responsive'],
     divideColor: ['responsive'],
@@ -679,6 +709,7 @@ module.exports = {
     order: ['responsive'],
     outline: ['responsive', 'focus'],
     overflow: ['responsive'],
+    overscrollBehavior: ['responsive'],
     padding: ['responsive'],
     placeholderColor: ['responsive', 'focus'],
     placeholderOpacity: ['responsive', 'focus'],
@@ -721,6 +752,7 @@ module.exports = {
     transitionTimingFunction: ['responsive'],
     transitionDuration: ['responsive'],
     transitionDelay: ['responsive'],
+    animation: ['responsive'],
   },
   corePlugins: {},
   plugins: [],
