3a0226a6a7056fb8361894ace1529486edb50cd2
Tim Kamanin <tim@timonweb.com>
2020-11-22 23:07:46 +0100

    Adds support for Tailwind css v2

---
+++ tailwind/app_template_v2/static_src/tailwind.config.js
@@ -0,0 +1,14 @@
+// This is a minimal config.
+// If you need the full config, get it from here:
+// https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
