d8af28a7f2a65dcfb052f58dc7d8f627df3e83f9
Tim Kamanin <tim@timonweb.com>
2020-12-13 14:25:52 +0100

    Updates dependencies; Enables purgecss by default

--- tailwind/app_template_v2/static_src/tailwind.config.js
+++ tailwind/app_template_v2/static_src/tailwind.config.js
@@ -2,13 +2,19 @@
 // If you need the full config, get it from here:
 // https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        // Templates within theme app (e.g. base.html)
+        '../templates/**/*.html',
+        // Templates in other apps. Uncomment the following line if it matches
+        // your project structure or change it to match.
+        // '../../templates/**/*.html',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
