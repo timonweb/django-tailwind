3937207a8db42d53fed41192d8b34edb1ed8ac4a
Tim Kamanin <tim@timonweb.com>
2021-04-26 09:16:37 +0200

    Update tailwind.config.js

--- src/tailwind/app_template_v2/{{cookiecutter.app_name}}/static_src/tailwind.config.js
+++ src/tailwind/app_template_v2/{{cookiecutter.app_name}}/static_src/tailwind.config.js
@@ -16,15 +16,22 @@ module.exports = {
 
     purge: [
         /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+         * HTML. Paths to Django template files that will contain Tailwind CSS classes (<tailwind_app_name>/templates).
          */
         /*  Templates within theme app (e.g. base.html) */
         '../templates/**/*.html',
 
-        /* Templates in other apps. Adjust the following line so that it matches
-         * your project structure.
+        /* 
+         * Main templates directory of the project (BASE_DIR/templates).
+         * Adjust the following line to match your project structure.
          */
         '../../templates/**/*.html',
+        
+        /* 
+         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+         * Adjust the following line to match your project structure.
+         */
+        '../../**/templates/**/*.html',
 
         /**
          * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
