65ce44e39a458be98686b12338e2aa53c2ce4f92
Tim Kamanin <tim@timonweb.com>
2021-12-09 23:58:57 +0100

    Updates example app

--- example/theme/static_src/tailwind.config.js
+++ example/theme/static_src/tailwind.config.js
@@ -6,15 +6,7 @@
  */
 
 module.exports = {
-    /**
-     * Stylesheet generation mode.
-     *
-     * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
-     * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
-     */
-    mode: "jit",
-
-    purge: [
+    content: [
         /**
          * HTML. Paths to Django template files that will contain Tailwind CSS classes.
          */
@@ -49,11 +41,7 @@ module.exports = {
          */
         // '../../**/*.py'
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
-    },
-    variants: {
         extend: {},
     },
     plugins: [
