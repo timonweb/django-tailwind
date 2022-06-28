d28ee32addd4f09ff47633070aa793627fb52313
Dhruva Shaw <dhruvashaw@gmail.com>
2022-06-28 22:50:56 +0530

    hopefully solving the node not found error

--- src/tailwind/npm.py
+++ src/tailwind/npm.py
@@ -20,18 +20,24 @@ class NPM:
         self.cwd = cwd
 
     def command(self, *args):
+        times_cwd_ran = 0 if args.get('times_cwd_ran') is None else args.get('times_cwd_ran')
         try:
             subprocess.run([self.npm_bin_path] + list(args), cwd=self.cwd, check=True)
             return True
         except subprocess.CalledProcessError:
             sys.exit(1)
         except OSError:
-            raise NPMException(
-                "\nIt looks like node.js and/or npm is not installed or cannot be found.\n\n"
-                "Visit https://nodejs.org to download and install node.js for your system.\n\n"
-                "If you have npm installed and still getting this error message, "
-                "set NPM_BIN_PATH variable in settings.py to match path of NPM executable in your system.\n\n"
-                ""
-                "Example:\n"
-                'NPM_BIN_PATH = "/usr/local/bin/npm"'
-            )
+            if times_cwd_ran>=1:
+                raise NPMException(
+                    "\nIt looks like node.js and/or npm is not installed or cannot be found.\n\n"
+                    "Visit https://nodejs.org to download and install node.js for your system.\n\n"
+                    "If you have npm installed and still getting this error message, "
+                    "set NPM_BIN_PATH variable in settings.py to match path of NPM executable in your system.\n\n"
+                    ""
+                    "Example:\n"
+                    'NPM_BIN_PATH = "/usr/local/bin/npm"'
+                )
+            else:
+                self.npm_bin_path = 'node'
+                self.command(times_cwd_ran=times_cwd_ran+1,*args)
+                return True
