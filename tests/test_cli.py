547d2a74cc5cb2c38f6c5b838747fed30838f2f8
Ryan Sevelj <2338952+rsevs3@users.noreply.github.com>
2025-01-27 01:27:29 +0930

    Fixed tests to work with modified tailwind config
    Signed-off-by: Ryan Sevelj <2338952+rsevs3@users.noreply.github.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,17 +9,21 @@ from .conftest import cleanup_theme_app_dir
 
 
 def test_tailwind_install_and_build(settings):
-    app_name = f'test_theme_{str(uuid.uuid1()).replace("-", "_")}'
+    app_name = f"test_theme_{str(uuid.uuid1()).replace('-', '_')}"
 
     call_command("tailwind", "init", "--app-name", app_name, "--no-input")
 
     settings.INSTALLED_APPS += [app_name]
     settings.TAILWIND_APP_NAME = app_name
 
-    assert os.path.isfile(os.path.join(get_app_path(app_name), "apps.py")), 'The "theme" app has been generated'
+    assert os.path.isfile(os.path.join(get_app_path(app_name), "apps.py")), (
+        'The "theme" app has been generated'
+    )
 
-    tailwind_config_path = os.path.join(get_app_path(app_name), "static_src", "tailwind.config.js")
-    assert os.path.isfile(tailwind_config_path), "tailwind.config.js is present"
+    tailwind_config_path = os.path.join(
+        get_app_path(app_name), "static_src", "package.json"
+    )
+    assert os.path.isfile(tailwind_config_path), "package.json is present"
 
     call_command("tailwind", "install")
     assert os.path.isdir(
