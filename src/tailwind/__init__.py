2b5a013b2310b9e101df93a481512e721e868797
SynthGhost <60333233+synthghost@users.noreply.github.com>
2025-12-04 22:27:54 -0500

    Fix settings-based CSS path configuration
    Move call to getattr out of the dictionary returned by get_config() so that later calls to tailwind_css_path all use the same value. Previously, setting TAILWIND_CSS_PATH to a non-default value would desynchronize the actual vs. rendered paths of the output CSS file.

--- src/tailwind/__init__.py
+++ src/tailwind/__init__.py
@@ -4,13 +4,13 @@ from django.conf import settings
 
 
 def get_config(setting_name):
-    tailwind_css_path = "css/dist/styles.css"
+    tailwind_css_path = getattr(settings, "TAILWIND_CSS_PATH", "css/dist/styles.css")
     return {
         "NPM_BIN_PATH": getattr(settings, "NPM_BIN_PATH", "npm"),
         # 'TAILWIND_DEV_MODE' is deprecated. Leaving it here
         # to support legacy browser-sync based configs.
         "TAILWIND_DEV_MODE": getattr(settings, "TAILWIND_DEV_MODE", False),
-        "TAILWIND_CSS_PATH": getattr(settings, "TAILWIND_CSS_PATH", tailwind_css_path),
+        "TAILWIND_CSS_PATH": tailwind_css_path,
         "TAILWIND_APP_NAME": getattr(settings, "TAILWIND_APP_NAME", None),
         "TAILWIND_USE_STANDALONE_BINARY": getattr(
             settings, "TAILWIND_USE_STANDALONE_BINARY", False
